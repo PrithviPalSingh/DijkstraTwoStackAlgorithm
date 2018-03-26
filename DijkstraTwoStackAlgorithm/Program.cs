a8c4301d942335b40bf72f90bab404b3b03bf59e
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-03-26 16:14:03 +0800

    Add project files.

---
+++ DijkstraTwoStackAlgorithm/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijkstraTwoStackAlgorithm
+{
+    /// <summary>
+    /// (1+((2+3)*(4*5)))
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine(); ;
+            Evaluate(str);
+            Console.Read();
+        }
+
+        private static void Evaluate(string str)
+        {
+            Stack<string> Operators = new Stack<string>();
+            Stack<int> Operands = new Stack<int>();
+
+            while (!string.IsNullOrWhiteSpace(str))
+            {
+                int value;
+                if (int.TryParse(str, out value))
+                {
+                    Operands.Push(value);
+                }
+                else
+                {
+                    if (str == "(")
+                    {
+                    }
+                    else if (str == ")" && Operands.Count > 1)
+                    {
+                        var item1 = Operands.Pop();
+                        var item2 = Operands.Pop();
+                        var op = Operators.Pop();
+
+                        switch (op)
+                        {
+                            case "+":
+                                Operands.Push(item1 + item2);
+                                break;
+                            case "*":
+                                Operands.Push(item1 * item2);
+                                break;
+                            case "/":
+                                Operands.Push(item1 / item2);
+                                break;
+                            case "-":
+                                Operands.Push(item1 - item2);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Operators.Push(str);
+                    }
+                }
+                str = Console.ReadLine();
+            }
+
+            Console.WriteLine(Operands.Pop());
+        }
+    }
+}
